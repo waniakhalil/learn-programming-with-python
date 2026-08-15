7b61619302d13cb3a4090f8eee00a60a27bc456c
waniakhalil <waniakhalilchaudhary@gmail.com>
2026-08-16 03:32:35 +0500

    adding file changingcase.py

---
+++ 09_TypeCasting/changingcase.py
@@ -0,0 +1,28 @@
+# Changing Casting in String Datatype
+# Check the datatype  of string
+print("Check the datatype Funtion:")
+print(type("Check the datatype Funtion"))
+
+#Output:
+#Check the datatype Funtion:
+#class <'str'>
+
+
+# Function of String for Changing Casting
+print("Function of String for Changing Casting:")
+name="Pakistan"
+print(name)
+print("1.Upper Function of String:")
+print(name.upper())
+print("2.Lower Function of String:")
+print(name.lower())
+print("3.Title Function of String:")
+print(name.title())
+
+
+
+#Output:
+#Function of String for Changing Casting:
+#1.Upper Function of String
+# Pakistan
+#PAKISTAN
