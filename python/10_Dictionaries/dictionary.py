555aaaca435073486c1e7696ca45ce3c7b20656b
waniakhalil <waniakhalilchaudhary@gmail.com>
2026-08-20 02:08:05 +0500

    Add dictionary.py: Introduce a new file explaining dictionaries in Python, including definitions, syntax, and an example of storing student information, enhancing educational content on data structures.

---
+++ python/10_Dictionaries/dictionary.py
@@ -0,0 +1,33 @@
+# Dictionary:
+# There are Three Ways to store different values or bunch of value to assign  one varible
+# 1.list [] it is way to verify this types 
+# 2.Tuples ()
+# 3.Dictionary {}
+
+# Definition:
+# Python provides a data structure Dictionary for such cases.Dictionary in simple words is a data structure which
+# stores the keyvalues pairs
+
+#Note:
+# 1.Please note that keys must be between single or double quotation.if they are string type ,every pair is separated from 
+# other pairs by a comma and every pair of key value is separated by colon
+
+# 2. if get value from dictionay get from key
+
+# 3. In dictionary not indexing but it key against work also pick random value also in loop 
+
+
+# syntax:
+# Dictionary name= {key:value,.............}
+# (key:value) is  called a pair  or element 
+
+
+# Example:
+# if you want to store the information about a student 
+print("Example of dictionary:")
+student={'Name':'Wania','Age':'20','Program':'Computer Science'}
+print(student)
+
+# Output:
+#Example of dictionary:
+# {'Name': 'Wania', 'Age': '20', 'Program': 'Computer Science'}
