5a276c05efc9161fec2219d391cade8a656b7284
waniakhalil <waniakhalilchaudhary@gmail.com>
2026-09-03 18:40:14 +0500

    addings a operation of  accessing membrane list of dictionary

--- python/10_Dictionaries/list_of_dictionary.py
+++ python/10_Dictionaries/list_of_dictionary.py
@@ -7,18 +7,34 @@
 #Create a list membrane of a dictionary 
 print("Syntax of Creating a list of dictionary:")
 print("[] list square bracket then a dictionary crual bracket{key:value}")
-mydic=[{'1.key:value','2.key:value','3.key:value',"...................."}]
+mydic=[{'1.key:':'value','2.key:':'value','3.key:':'value'}]
 print(mydic,'\n')
 print("Example of Creating a list of dictionary:")
 students=[{'Name:':'Wania','Age:':'20','Gender:':'Female'}]
-print(students)
+print(students,'\n')
 
 #Output:
 #Syntax of Creating a list of dictionary:
 #[] list square bracket then a dictionary crual bracket{key:value}
-#[{'1.key:value', '2.key:value', '3.key:value', '....................'}]
+#[{'1.key:value', '2.key:value', '3.key:value'}]
 
 #Example of Creating a list of dictionaries:
 #[{'Name:': 'Wania', 'Age:': '20', 'Gender:': 'Female'}]
 
-# 
+# <<<<<<<<<<<<<<<<<<<<<<Perform list Operation on Creating a list of dictionary>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#Accessing list membrane from a list of dictionary
+print("1.Syntax of Accessing list membrane from a list of dictionary")
+print("list name [index of list][dictionary key name]")
+print(mydic[0]['1.key:'],'\n')
+print("1.Example of Accessing list membrane from a list of dictionary")
+print(students[0]['Name:'],'\n')
+
+#Output:
+#1.Syntax of Accessing list membrane from a list of dictionary
+#list name [index of list][dictionary key name]
+#value
+
+#1.Example of Accessing list membrane from a list of dictionary
+#Wania
+
+
