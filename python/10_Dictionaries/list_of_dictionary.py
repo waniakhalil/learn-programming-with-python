52c9c5e2f7e7986692db9c8bac4783324b4b7b72
waniakhalil <waniakhalilchaudhary@gmail.com>
2026-09-03 18:00:13 +0500

    addings a example  of creating a list of  dictionaries

--- python/10_Dictionaries/list_of_dictionary.py
+++ python/10_Dictionaries/list_of_dictionary.py
@@ -1,4 +1,14 @@
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Creating a list of dictionaries >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #To mimic a database we can create a list of dictionary ,where list be an in memory database
 #  and each dictionary object will represent a unique record 
-# we create a list members means key value pair is dictionary 
+# create a list members means key value pair is dictionary 
+
+# Example of Creating a list of dictionaries
+#Create a list membrane of a dictionary 
+print("Example of Creating a list of dictionaries:")
+students=[{'Name:':'Wania','Age:':'20','Gender:':'Female'}]
+print(students)
+
+#Output:
+#Example of Creating a list of dictionaries:
+#[{'Name:': 'Wania', 'Age:': '20', 'Gender:': 'Female'}]
