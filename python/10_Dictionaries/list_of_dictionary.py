7420f52f7f5561b34d469ea386bc1414a0b504d8
waniakhalil <waniakhalilchaudhary@gmail.com>
2026-09-03 18:14:48 +0500

    adding syntax of creating a list in dictionary

--- python/10_Dictionaries/list_of_dictionary.py
+++ python/10_Dictionaries/list_of_dictionary.py
@@ -5,10 +5,20 @@
 
 # Example of Creating a list of dictionaries
 #Create a list membrane of a dictionary 
-print("Example of Creating a list of dictionaries:")
+print("Syntax of Creating a list of dictionary:")
+print("[] list square bracket then a dictionary crual bracket{key:value}")
+mydic=[{'1.key:value','2.key:value','3.key:value',"...................."}]
+print(mydic,'\n')
+print("Example of Creating a list of dictionary:")
 students=[{'Name:':'Wania','Age:':'20','Gender:':'Female'}]
 print(students)
 
 #Output:
+#Syntax of Creating a list of dictionary:
+#[] list square bracket then a dictionary crual bracket{key:value}
+#[{'1.key:value', '2.key:value', '3.key:value', '....................'}]
+
 #Example of Creating a list of dictionaries:
 #[{'Name:': 'Wania', 'Age:': '20', 'Gender:': 'Female'}]
+
+# 
