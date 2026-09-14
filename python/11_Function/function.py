76ed29e768fe7450b8452d312ce1264e2b7914c1
waniakhalil <waniakhalilchaudhary@gmail.com>
2026-09-15 02:37:57 +0500

    Add function definition, syntax, and example for a simple addition operation, enhancing understanding of function structure in Python.

--- python/11_Function/function.py
+++ python/11_Function/function.py
@@ -12,3 +12,21 @@
 
 # For Example:
 # If we given task perform many times we write one time in function then we need to use this function we call many times the function
+
+# Function Syntax or define:
+print("Function Syntax or define:")
+print("In python we define a function with a keyword def then the function name after the name of function we supply pair of  parentheses and a colon sign")
+
+# Syntax Example:
+
+# def is definition of function add is a function name () small brackets  : colon is block of statement 
+# after block is body of function start 
+print("Syntax Example:")
+print("Function of Adds two number get input from user:")
+def add():
+     number1=int(input('Enter a value:'))
+     number2=int(input('Enter a value:'))
+     print(number1+ number2)
+print(add())
+
+# Note: Every Statement which is part of function body is a level indend more than the definition of function.
