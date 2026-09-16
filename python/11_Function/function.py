1149f3293e7d96d7a32fa5c9916f891ff3a118bc
waniakhalil <waniakhalilchaudhary@gmail.com>
2026-09-16 23:26:36 +0500

    Call a function  Method

--- python/11_Function/function.py
+++ python/11_Function/function.py
@@ -1,32 +1,47 @@
 # Function:
-# What is function ,why use ,importantance and used ?
+# What is function,why use ,importance and used ?
 
 #Function are a way to achieve the modularity and reuseability in code.
 
 # What is modularity?
 # Modularity programming is the process of subdividing a computer program into separate sub-programs.
 # A module can often be used in a variety of applications and functions with other components of the system.
+# modularity means create module in which many functions when we need the function in module we call the function we need and used 
 
 # What is Reuseability?
 # Using of already developed code according to our requirement without writing from the sractch
 
-# For Example:
-# If we given task perform many times we write one time in function then we need to use this function we call many times the function
+# For Example of resuseability:
+# If we given task perform many times we write one time in function then we need to use this function we call many times the function and the function perform task 
 
 # Function Syntax or define:
 print("Function Syntax or define:")
 print("In python we define a function with a keyword def then the function name after the name of function we supply pair of  parentheses and a colon sign")
 
-# Syntax Example:
+# Syntax or definition of function Example:
 
-# def is definition of function add is a function name () small brackets  : colon is block of statement 
-# after block is body of function start 
-print("Syntax Example:")
-print("Function of Adds two number get input from user:")
+# def is definition of function add is a function name () small brackets or braces  : colon is indicate block of statement 
+# after block is body of function start in the wirte of code in function we call function write code in function is execute
+# function is slient mode then we call the function 
+print("Syntax or definition of funtion Example:")
+print("Function of Adds two number get input from user:","\n")
 def add():
      number1=int(input('Enter a value:'))
      number2=int(input('Enter a value:'))
      print(number1+ number2)
-print(add())
 
 # Note: Every Statement which is part of function body is a level indend more than the definition of function.
+
+# How to call a function:
+# to call the function we just need to write the name of function followed by parathenes 
+add() # call the function of adds two number get input from user
+
+#Output:
+#Function Syntax or define:
+#In python we define a function with a keyword def then the function name after the name of function we supply pair of  parentheses and a colon sign
+#Syntax or definition of funtion Example:
+#Function of Adds two number get input from user:
+
+#Enter a value:45 
+#Enter a value:67
+#112
