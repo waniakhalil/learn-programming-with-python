b520038af2b90d179ea9d64da2f3663b3d81d848
waniakhalil <waniakhalilchaudhary@gmail.com>
2026-09-21 02:25:39 +0500

    adds the defintion , syntax and example of while loop

--- python/08_Loops/whileloop.py
+++ python/08_Loops/whileloop.py
@@ -1,8 +1,52 @@
 #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<While loop>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-#We have studied loops in earier ways like (nested and for loop). There is another type of loops called while loop
+# Definition and why use and importance of loop in for loop folder 
 
-#They are similar to for loops but differ in the sense that it allows user to terminate loop  y setting flag 
+#We have studied loops in earier ways like (nested and for loop). There is another type of loops called while loop
+# Defintion:
+#They are similar to for loops but differ in the sense that it allows user to terminate loop  setting flags
+# While loop also used iterate or repeadly any task 
+# functionality same of for and while loop 
+
+# Difference between for and while loop:
+
+# for loop start and end of sudden point of during excution
+# While loop  provide chocie to user loop terminate any sudden point or end of excution of loop
+
+# For termination using flags is variable we set a condition in while loop if condition come true or fale terminate
+
+
+#Syntax of While loop:
+
+#  keyword
+# While     Conditon:
+#        statemtent
+
+print("Syntax of While loop:") 
+print('while loop key word while then condition (:) colon start of block of statement or body of loop then given one or more statemnent',"\n")
+
+print("Example of While loop:")
+
+a=0
+while a<10: # While loop condition is false is terminate when condition is true is excute 
+    print(a,"This is a while loop") 
+    a+=1
+
+# Output:
+#Syntax of While loop:
+#while loop key word while then condition (:) colon start of block of statement or body of loop then given one or more statemnent 
+
+#Example of While loop:
+#0 This is a while loop
+#1 This is a while loop
+#2 This is a while loop
+#3 This is a while loop
+#4 This is a while loop
+#5 This is a while loop
+#6 This is a while loop
+#7 This is a while loop
+#8 This is a while loop
+#9 This is a while loop
 
 
 
