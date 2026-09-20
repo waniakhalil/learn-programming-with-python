fcc4e7da87316b7d6ed48f341225b82189247303
waniakhalil <waniakhalilchaudhary@gmail.com>
2026-09-21 02:36:25 +0500

    Enhances for loop documentation with improved definitions, syntax corrections, and additional examples

--- python/08_Loops/for_loop.py
+++ python/08_Loops/for_loop.py
@@ -1,15 +1,20 @@
-# Loop:
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Loop>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 # Definition:
 # Any task repeated or repeatly perform task is called loop
 # if one code write many times is solution is loop
-# for example : if your task is print you name 50 times you code write 50 times is time confusing and not flexiable best  and fescant way is using loop 
+
+# For example : if your task is print you name 50 times you code write 50 times is time confusing and not flexiable best  and fescant way is using loop 
 # loop have starting and ending point  tells how many times loop run
 # loop execute task perform
 
-# types of loop:
+# Types of loop:
 # In python we study two types of loops :
 # 1.for loop
 # 2.while loop
+#3.nested loop 
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<For loop>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # for loop syntax:
 # for(keyword) element or variablein( list name) :
@@ -29,12 +34,13 @@
 #: indicates the block of loop and start of body of loop
 # indentation is used to indicate the block of loop
 
-# Example of syntax:
-print('Example of for loop synatx and print you name 5 times using for loop:','\n')
+# Example of  for loop print you name 5 times using for loop::
+print('Example of for loop print you name 5 times using for loop:','\n')
 for a in range(5):# 5 will not be included in loop (0,1,2,3,4) it start from 0 and end at 4
     print(a,"wania",'\n') # a indicates loop execution how many times loop run 
 
 # Output:
+
 #Example of for loop synatx and print you name 5 times using for loop:
 # 0 wania 
 # 1 wania 
@@ -48,6 +54,7 @@ for numbers in range(10):
     print(numbers,'\n')
 
 # Output:
+
 #Example of range in single parameter given in for loop:
 #0 
 #1
@@ -66,6 +73,7 @@ for numbers in range(1,10):#  1 is a starting point and 10 is ending point of lo
     print(numbers,'\n')
 
 # Output:
+
 #Example of range in which starting and ending point of two parameter given in loop:'
 #1
 #2
@@ -84,6 +92,7 @@ for number in range(1,10,2): # 2 indicates step third parameter
     print(number,'\n')
 
 # Output:
+
 #Example of Range is step in three parameter  given in loop:
 #1
 #3
@@ -97,6 +106,7 @@ for b in range(10,1,-2):
     print(b,'\n')
 
 # Output:
+
 #Example of Reverse Number Print by loop
 #10
 #8
@@ -111,6 +121,7 @@ for city in cities:
     print(f'The city is:{city}','\n')
 
 #Output:
+
 # The list of cities acesses using loop:['karachi','lahore','Islamabad']
 # The city is:karachi
 # The city is:lahore
@@ -122,6 +133,7 @@ for num in [11,22,33,44,55]:
     print(num,'\n')
 
 # Output:
+
 #'Direct given list in loop:'
 # 11
 #22
@@ -136,6 +148,7 @@ for char in country:
     print(char)
 
 # Output:
+
 # Print Character using loop
 # Pakistan
 
@@ -146,11 +159,13 @@ for tup in country:
     print(tup)
 
 # Output:
+
 #Tuples using loop:
 # Pakistan
 # China
 # Japan
 
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Loops two ways two stop>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # In for loop study of break and continue:
 #   Example of break:
@@ -163,6 +178,7 @@ for num in range(10):
     print(num,'\n')
 
 # Output:
+
 # Using break in loop
 #1.Example 0 is unequal and not  divisible by 3 
 # 0
@@ -174,6 +190,7 @@ for num in range(10):
    print(num,'\n')
 
 #Output:
+
 # 2.Example 1 is unequal and not  divisible by 3 
 # 0
 
@@ -184,6 +201,7 @@ for num in range(10):
    print(num,'\n')
 
 #Output:
+
 # 3.Example 0 is equal and not divisible by 3
 # not print 
 
@@ -194,6 +212,7 @@ for num in [5,7,11,90]:
     print(num,'\n')
 
 # Output:
+
 #4.Example 0 is equal and not divisible by 2
 # 5
 #7
@@ -207,6 +226,7 @@ for num in range(10):
     print(num,'\n')
 
 #Output:
+
 #Using Contine in loop
 # Example check according given range and condition
 #0
@@ -227,6 +247,7 @@ for a in range(1,11):
     print(f"{tablenumber} * {a} = {tablenumber*a}")
 
 #Output:
+
 # Real World Example of for loop:
 #Print table using for loop and Input from user:
 #Enter a number for table print:4
