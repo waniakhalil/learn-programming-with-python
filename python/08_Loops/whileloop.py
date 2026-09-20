68157b3004f9eef2a645407c6e55af4a6004c3ae
waniakhalil <waniakhalilchaudhary@gmail.com>
2026-09-21 01:49:58 +0500

    adds the file of whileloop in loop

---
+++ python/08_Loops/whileloop.py
@@ -0,0 +1,8 @@
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<While loop>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+#We have studied loops in earier ways like (nested and for loop). There is another type of loops called while loop
+
+#They are similar to for loops but differ in the sense that it allows user to terminate loop  y setting flag 
+
+
+
