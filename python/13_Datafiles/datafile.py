c9c4d3fe89bdcd84575eed440c102a19ef8ebc2d
waniakhalil <waniakhalilchaudhary@gmail.com>
2026-09-25 00:16:47 +0500

    adds the folder of datafile

---
+++ python/13_Datafiles/datafile.py
@@ -0,0 +1,17 @@
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Data File>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# In all the coding so far in this book,none of the data has been perserved .we created variables,list ,dictionaries,loop
+# and class instance that contain information,but as soon as the computer or program was turned off ,all of it disappeared
+# Variable is not permenant generate
+# if finish program save in the data file 
+
+#you known  how to save a word processing document or spreadsheet ,but how do you save data processing by python ?
+# we see external file program transfer to another file
+
+# Reading Writing in an external file from python code 
+
+# We can write in a text file by using a python function:
+# With open("file_name.txt,"mode")
+
+# There are three modes:
+# Read,Write and append
